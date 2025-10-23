1d922be09a100b81a90e2360450a118e09e5a963
Gabriel Nascimento <ogabrielnascimento@outlook.com>
2025-10-22 21:42:49 -0300

    Introdução React

---
+++ exemplo-react/src/Titulo.jsx
@@ -0,0 +1,10 @@
+function Titulo({titulo = "Exemplo React"}){
+    return (
+        <div className="container">
+
+        <h1 ClassName= "text-primary">{titulo}</h1>
+        </div>
+    )
+}
+
+export default Titulo;
